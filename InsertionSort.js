4efcd50595eb78f068ce45a7a7f4e9e722b7d429
Charlotte Astle <charlotteastle@Charlottes-MacBook-Pro.local>
2017-08-17 11:24:21 -0700

    selection sort

--- InsertionSort.js
+++ InsertionSort.js
@@ -1,12 +1,10 @@
-var arraySize = 50;
-
-// Case #2 - Large set of numbers
-//var arraySize = 40000 
+var arraySize = 40;
 
 var array = [];
-for (var index = 0; index < arraySize; index++) {
-	var randomNumber = Math.round(Math.random()*arraySize);
 
+//Generate array of random numbers
+for (var index = 0; index , arraySize; index++) {
+	var randomNumber = Math.round(Math.random()*arraySize);
 	array.push(randomNumber);
 }
 
@@ -16,24 +14,24 @@ function swap(items, firstIndex, secondIndex) {
 	items[secondIndex] = temp;
 }
 
-function selectionSort(items) {
+function insertionSort(items) {
 
 	var len = items.length;
 	var min;
 
-	for(var i = 0; i < len; i++) {
-		//set minimum index to this position
+	for(var i=0; i<len; i++) {
+		// set this to minimum position
 		min = i;
 
-		//see if any other number in the array is smaller than this one
-		for(var j = i+1; j <len; j++) {
+		//see if any numbers behind current index are less that current index
+		for (var j = i - 1; j >= 0; j--) {
 			if(items[j] < items[min]) {
-				min = j;
+				min = j
 			}
 		}
 
-		//if current position isn't minimum, swap the numbers
-		if (i !== min) {
+		//swap the new min with the current element
+		if(i !== min) {
 			swap(items, i, min);
 		}
 	}
